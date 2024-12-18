1bbbd345ac25231786c86059f17b1cf7ccbe65bb
Roee Fligman <fligman1@gmail.com>
2024-12-18 16:20:29 +0200

    app organize

---
+++ src/utils/apiFetcher.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const API_URL = "";
+
+export const getData = async (saveData) => {
+    try {
+        const res = await axios.get(API_URL);
+        saveData(res.data);
+    } catch (err) {
+        toast.error(err.message, { theme: "colored" });
+    }
+}

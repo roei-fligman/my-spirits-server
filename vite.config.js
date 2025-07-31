c8ed551a94afd561e70294d6fbf94d4eb0c39516
Roee Fligman <fligman1@gmail.com>
2025-07-31 17:32:02 +0300

    fix

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-export default defineConfig({
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern-compiler' // or "modern"
-            }
-        }
-    }
-})
